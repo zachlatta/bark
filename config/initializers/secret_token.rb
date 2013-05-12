94b14a348700f8a89aee1769bc41956cc5c0543b
Zach Latta <zchlatta@gmail.com>
2013-05-12 14:10:00 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bark::Application.config.secret_token = '441bf0ffc42f44a968805ebe4ef51204f07cf608989316c3d7061a199502e93afb41bf7cba5830c8e26a499a77ce2049fbda3bcfed0c88914d7002787e83fd46'
