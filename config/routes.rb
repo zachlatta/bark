fcc868defdd601a13020f790f014955c9dcf30d7
Zach Latta <zchlatta@gmail.com>
2013-05-12 16:41:39 -0700

    Finish static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Bark::Application.routes.draw do
   get "static_pages/home"
-
   get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the h1 'Bark App'" do
+
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Bark')
+    end
+
+    it "should have the right title" do
+
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Home | Bark")
+    end
+  end
+
+  describe 'Help page' do
+
+    it "should have the h1 'Help'" do
+
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => "Help")
+    end
+
+    it "should have the right title" do
+
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Help | Bark")
+    end
+  end
+
+  describe 'About page' do
+
+    it "should have the h1 'About Us'" do
+
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the right title" do
+
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "About Us | Bark")
+    end
+  end
+
+  describe 'Contact page' do
+
+    it "should have the h1 'Contact'" do
+
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => 'Contact | Bark')
+    end
+  end
+end
