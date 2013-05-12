37dd50165b7679ab2152bb8895e4294ed7b619d3
Zach Latta <zchlatta@gmail.com>
2013-05-12 14:49:31 -0700

    Add a StaticPages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Bark::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
